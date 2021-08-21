db0052c216c4ecdb5ab262151cdeecefb2ab5190
JuanCalle1606 <jpmilojp@gmail.com>
2021-08-20 22:34:38 -0500

    Update to v4.8

--- native_extension/ane/build-windows.bat
+++ native_extension/ane/build-windows.bat
@@ -1,7 +1,7 @@
 REM Get the path to the script and trim to get the directory.
 @echo off
 SET SZIP="C:\Program Files\7-Zip\7z.exe"
-SET AIR_PATH="C:\Users\usuario\Desktop\Programacion\SDK\AIRSDK_Compiler\bin"
+SET AIR_PATH="C:\Users\User\source\SDK\AIRSDK_Windows\bin\"
 echo Setting path to current directory to:
 SET pathtome=%~dp0
 echo %pathtome%
